2f53aa013d46b71a3d5f13c3bf2bde14dc2279ee
poupouenpls <aurel.hasteur@gmail.com>
2025-02-10 21:25:43 +0100

    feat: ajout de pages mentions

---
+++ front-end/src/components/cgu.jsx
@@ -0,0 +1,13 @@
+const CGU = () => {
+    return (
+        <div className="p-[24px]">
+            <h1 className="font-sans text-[32px] font-bold mb-[20px]">Conditions Générales Utilisation</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras cursus non velit quis aliquet. Suspendisse potenti.
+            Nullam faucibus nulla ut diam semper, ut bibendum ante sagittis. Vestibulum arcu tellus, cursus et vulputate eu, tincidunt a nisl.
+            Aenean congue ligula a arcu finibus, sit amet dictum eros vulputate.</p>
+        </div>
+    )
+}
+
+export default CGU
