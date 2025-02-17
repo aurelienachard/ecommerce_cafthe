74694c2af7f1e31367e0a23cbb67f11f4fe4f852
poupouenpls <aurel.hasteur@gmail.com>
2025-02-17 14:31:58 +0100

    feat: ajout de la recuperation des donnees sur la page commande

--- front-end/src/components/profileCommande.jsx
+++ front-end/src/components/profileCommande.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 const ProfilCommande = () => {
+    const [commande, setCommande] = useState([])
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+
+        axios.get("http://localhost:3001/orders", {
+            headers: {
+                'Authorization' : `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            setCommande(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, [])
+
     return (
-        <div className="min-h-screen flex flex-col p-[24px]">
-            <h1 className="text-[32px] font-[Roboto] mb-[24px] font-bold">Voir les commandes</h1>
+        <div>
+            <h1>Commandes</h1>
+
+            {commande.map(order => {
+                <div>
+                    <p>Order ID: {order.orders_id}</p>
+                    <p>Status: {order.order_status}</p>
+                    <p>Products: {JSON.stringify(order.product_details)}</p> 
+                    {/* on demande a afficher les produits au format json */}
+                </div>
+            })}
+
         </div>
     )
 }
