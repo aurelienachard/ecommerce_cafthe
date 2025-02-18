b64364e194028d762bb908e1f664843a514c025c
poupouenpls <aurel.hasteur@gmail.com>
2025-02-18 11:59:03 +0100

    fix: mis en commentaire les problems avec map en attendant

--- front-end/src/components/profileCommande.jsx
+++ front-end/src/components/profileCommande.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react"
 import axios from 'axios'
 
 const ProfilCommande = () => {
-    const [commande, setCommande] = useState([])
+    // const [commande, setCommande] = useState([])
 
     useEffect(() => {
-        const token = localStorage.getItem('token')
-
+        const token = localStorage.getItem('token');
+    
         axios.get("http://localhost:3001/orders", {
             headers: {
                 'Authorization' : `Bearer ${token}`, // on veut le token
@@ -14,22 +14,23 @@ const ProfilCommande = () => {
             }
         })
         .then(response => {
-            setCommande(response.data)
+            console.log(response.data); // Ajoutez ceci pour vérifier la structure de la réponse
+            setCommande(response.data);
         })
         .catch(error => {
-            console.log(error)
-        })
-    }, [])
+            console.log(error);
+        });
+    }, []);
 
     return (
         <div className="p-[16px]">
             <h1 className="font-bold text-[32px] mb-[20px]">Commandes</h1>
-            {commande.map(order => (
+            {/* {commande.map(order => (
                 <div key={order.orders_id}>
                     <p>OrderID: {order.orders_id}</p>
                     <p>Status: {order.order_status}</p>
                 </div>
-            ))}
+            ))} */}
         </div>
     )
 }
