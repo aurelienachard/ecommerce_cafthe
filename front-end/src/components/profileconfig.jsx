c23899e5917b5e1d90ecfa26673ed6ac0ab4a0ac
poupouenpls <aurel.hasteur@gmail.com>
2025-02-18 10:10:07 +0100

    feat: affichage des informations personnelles dans les inputs

--- front-end/src/components/profileconfig.jsx
+++ front-end/src/components/profileconfig.jsx
@@ -1,13 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from 'axios'
 
 const ProfileConfig = () => {
     const [utilisateurs_nom, set_utilisateurs_nom] = useState('')
     const [utilisateurs_prenom, set_utilisateurs_prenom] = useState('')
     const [utilisateurs_adresse_email, set_utilisateurs_adresse_email] = useState('')
-    const [utilisateurs_mot_de_passe, set_utilisateurs_mot_de_passe] = useState('')
     const [utilisateurs_numero_de_telephone, set_utilisateurs_numero_de_telephone] = useState('')
     const [message, setMessage] = useState('')
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        // verification du token
+        if (token) {
+            axios.get('http://localhost:3001/utilisateurs/profil', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then((response) => {
+                console.log('Données utilisateur récupérées:', response.data); // Vérifiez les données ici
+
+                /*
+    
+                    (|| '') il permet de definir les valeurs initiales des etats pour ne dire qu'ils ne sont jamais undefined
+                    on utilise '' une chaine vide comme valeur par defaut
+    
+                    Entrée contrôlée : Une entrée est dite contrôlée lorsque sa valeur est gérée par l'état du composant. Cela signifie que la valeur de l'entrée est toujours synchronisée avec l'état du composant.
+                    Entrée non contrôlée : Une entrée est non contrôlée lorsque sa valeur est gérée par le DOM lui-même, sans synchronisation avec l'état du composant.
+    
+                    Valeurs de secours : En utilisant userData.utilisateurs_nom || '', vous vous assurez que même si userData.utilisateurs_nom est undefined, l'état sera mis à jour avec une chaîne vide, évitant ainsi le passage à une entrée non contrôlée.
+                */
+    
+                    const userData = response.data;
+                    set_utilisateurs_nom(userData.utilisateurs_nom || '');
+                    set_utilisateurs_prenom(userData.utilisateurs_prenom || '');
+                    set_utilisateurs_adresse_email(userData.utilisateurs_adresse_email || '');
+                    set_utilisateurs_numero_de_telephone(userData.utilisateurs_numero_de_telephone || '');
+                })
+                .catch((error) => {
+                    console.log('Erreur lors de la récupération des données:', error);
+                    setMessage('Erreur de récupération des données');
+                });
+        } else {
+            console.log('token non trouve')
+        }
+    }, []) // le tableau vide est pour que useeffect se lance une fois
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -17,7 +54,6 @@ const ProfileConfig = () => {
             utilisateurs_nom,
             utilisateurs_prenom,
             utilisateurs_adresse_email,
-            utilisateurs_mot_de_passe,
             utilisateurs_numero_de_telephone,
         }
 
@@ -48,10 +84,6 @@ const ProfileConfig = () => {
         set_utilisateurs_adresse_email(event.target.value)
     }
 
-    const handleChangeMotDePasse = (event) => {
-        set_utilisateurs_mot_de_passe(event.target.value)
-    }
-
     const handleChangeNumeroDeTelephone = (event) => {
         set_utilisateurs_numero_de_telephone(event.target.value)
     }
@@ -76,18 +108,13 @@ const ProfileConfig = () => {
                         <label className="mt-[20px]">Adresse Email</label>
                         <input value={utilisateurs_adresse_email} onChange={handleChangeAdresseEmail} placeholder="Adresse Email" type="text" className="border bg-white p-[16px] mt-[10px] w-150"/>
                     </div>
-                    
-                    <div className="flex flex-col">
-                        <label className="mt-[20px]">Mot de passe</label>
-                        <input value={utilisateurs_mot_de_passe} onChange={handleChangeMotDePasse} placeholder="Mot de passe" type="text" className="border bg-white p-[16px] mt-[10px] w-150"/>
-                    </div>
 
                     <div className="flex flex-col">
                         <label className="mt-[20px]">Numero de telephone</label>
                         <input value={utilisateurs_numero_de_telephone} onChange={handleChangeNumeroDeTelephone} placeholder="Numero de telephone" type="text" className="border bg-white p-[16px] mt-[10px] w-150"/>
                     </div>
 
-                    <button className="bg-green-principale p-[16px] mt-[24px] text-white w-150">Mettre a jour</button>
+                    <button type="submit" className="bg-green-principale p-[16px] mt-[24px] text-white w-150">Mettre a jour</button>
                 
                     <p>{message}</p>
                 </form>
