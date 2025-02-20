d427e230c692e3a50d03a36b68ae15da65455370
poupouenpls <aurel.hasteur@gmail.com>
2025-02-20 19:02:18 +0100

    feat: ajout du nom de la personne

--- front-end/src/components/profile.jsx
+++ front-end/src/components/profile.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import axios from 'axios'
 
 const Profile = () => {
+    const [username, setUsername] = useState()
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        axios.get('http://localhost:3001/utilisateurs/profil', {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            } 
+        })
+        .then(response => {
+            setUsername(response.data.utilisateurs_nom)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, [])
+
     return (
         <div className="min-h-screen p-[24px]">
             <h1 className="text-[32px] font-bold">Profile</h1>
+
+            <p className="text-[24px]">Bonjour, {username}!</p>
 
             <div className="flex flex-col">
                 <Link to="/fortgetpwd" className=" mt-[16px] text-green-principale">Modifier son mot de passe</Link>
