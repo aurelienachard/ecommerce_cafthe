d49cfd4d4659ed3d0a0d8e02ac9f3d5033a7ccaf
poupouenpls <aurel.hasteur@gmail.com>
2025-02-21 12:28:29 +0100

    feat: ajout du design pour les commandes

--- front-end/src/components/profileCommande.jsx
+++ front-end/src/components/profileCommande.jsx
@@ -1,36 +1,71 @@
-import { useEffect } from "react"
+import {useState, useEffect} from 'react'
 import axios from 'axios'
 
 const ProfilCommande = () => {
-    // const [commande, setCommande] = useState([])
+    const [orders, setOrders] = useState([])
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
-    
-        axios.get("http://localhost:3001/orders", {
+        const token = localStorage.getItem('token')
+        axios.get('http://localhost:3001/orders', {
             headers: {
-                'Authorization' : `Bearer ${token}`, // on veut le token
-                'Content-Type' : 'application/json', // on veut le format json
+                'Authorization': `Bearer ${token}`  
             }
         })
-        .then(response => {
-            console.log(response.data); // Ajoutez ceci pour vérifier la structure de la réponse
-            // setCommande(response.data);
+        .then((response) => {
+            setOrders(response.data)
         })
-        .catch(error => {
-            console.log(error);
-        });
-    }, []);
+        .catch((error) => {
+            console.log(error)
+        })
+    }, [])
 
     return (
-        <div className="p-[16px]">
-            <h1 className="font-bold text-[32px] mb-[20px]">Commandes</h1>
-            {/* {commande.map(order => (
-                <div key={order.orders_id}>
-                    <p>OrderID: {order.orders_id}</p>
-                    <p>Status: {order.order_status}</p>
+        <div className="p-[24px]"> 
+            <h1 className="font-bold mb-[20px] text-gray-800">Mes Commandes</h1>
+
+            {orders.length === 0 ? (
+                <p>Vous n&apos;avez pas encore de commandes.</p>
+            ) : (
+                <div>
+                    {orders.map((order) => {
+                        const products = order.product_details
+                        const totalPrix = products.reduce((sum, product) => sum + (product.prix * product.quantite), 0)
+
+                        return (
+                            <div key={order.order_id} className='border p-[24px] rounded-md shadow-md mt-[20px]'>
+                                <div>
+                                    <div className="mb-[20px]">
+                                        <div className='flex justify-between'>
+                                            <p>Commande #{order.orders_id}</p>
+                                            <p className="py-[12px] px-[16px] bg-orange-300 rounded-md">Status: {order.order_status}</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    {products.map((product, index) => (
+                                        <div key={index} className='flex justify-between'>
+                                            <div className='flex'>
+                                                <p className='mr-[60px]'>{product.nom}</p>
+                                                <p>Quantité: {product.quantite}</p>
+                                            </div>
+                                            <p>{product.prix}€</p>
+                                        </div>
+                                    ))}
+                                </div>
+                                
+                                <div className='border mt-[20px] mb-[20px]'></div>
+
+                                <div className='flex justify-between'>
+                                    <p>Total</p>
+                                    <p>{totalPrix.toFixed(2)}€</p>
+                                </div>
+
+                            </div>
+                        )
+                    })}
                 </div>
-            ))} */}
+            )}
         </div>
     )
 }
