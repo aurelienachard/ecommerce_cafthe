837ff3183a2f03c9a91b0ffddff9e37d872ca485
poupouenpls <aurel.hasteur@gmail.com>
2025-03-09 18:47:42 +0100

    feat: ajout composant affichage produit panier

--- front-end/src/pages/panier.jsx
+++ front-end/src/pages/panier.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import axios from 'axios'
-
-import { XMarkIcon } from '@heroicons/react/20/solid'
+import CartItems from '../components/CartItems'
 
 const Panier = () => {
     const [panier, setPanier] = useState([])
@@ -121,66 +120,12 @@ const Panier = () => {
                         ) : (
                             <form className="mt-12">
                                 <div className="flex flex-col lg:flex-row lg:gap-x-12 xl:gap-x-16">
-                                    <div className="flex-1">
-                                        <div className="border-t border-b border-gray-200">
-                                            {panier.map(item => 
-                                                <div key={item.id} className="flex py-[24px] sm:py-[40px]">
-                                                    <div className="shrink-0">
-                                                        <img src="./image.jpg"
-                                                            alt={item.nom}
-                                                            className="size-24 rounded-md object-cover sm:size-48"
-                                                        />
-                                                    </div>
-
-                                                    <div className="ml-4 flex flex-1 flex-col justify-between sm:ml-6">
-                                                        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                                                            <div>
-                                                                <div className="flex justify-between">
-                                                                    <h3 className="text-sm">
-                                                                        <span className="font-medium text-gray-700 hover:text-gray-800">
-                                                                            {item.nom}
-                                                                        </span>
-                                                                    </h3>
-                                                                </div>
-                                                                <p className="mt-1 text-sm text-gray-500">{item.quantiteGramme}</p>
-                                                                <p className="mt-1 text-sm font-medium text-gray-900">${item.prix}</p>
-                                                            </div>
-
-                                                            <div className="mt-4 sm:mt-0 sm:pr-9">
-                                                                <div className="flex items-center space-x-3">
-                                                                    <button
-                                                                        type="button"
-                                                                        onClick={() => diminuerArticle(item.id, item.quantiteGramme)}
-                                                                        className="rounded-md bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm border hover:bg-gray-50">
-                                                                        -
-                                                                    </button>
-
-                                                                    <p className="text-sm text-gray-900">{item.quantite}</p>
-
-                                                                    <button
-                                                                        type="button"
-                                                                        onClick={() => augmenterArticle(item.id, item.quantiteGramme)}
-                                                                        className="rounded-md bg-white px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm border hover:bg-gray-50">
-                                                                        +
-                                                                    </button>
-                                                                </div>
-
-                                                                <div className="absolute right-0 top-0">
-                                                                    <button
-                                                                        type="button" 
-                                                                        onClick={() => supprimerArticle(item.id, item.quantiteGramme)}
-                                                                        className="-m-2 inline-flex p-2 text-gray-400 hover:text-gray-500">
-                                                                        <span className="sr-only">Supprimer</span>
-                                                                        <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            )}
-                                        </div>
-                                    </div>
+                                    <CartItems 
+                                        panier={panier}
+                                        augmenterArticle={augmenterArticle}
+                                        diminuerArticle={diminuerArticle}
+                                        supprimerArticle={supprimerArticle}
+                                    />
 
                                     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:mt-0 lg:w-96">
                                         <h2 className="text-lg font-medium text-gray-900">
