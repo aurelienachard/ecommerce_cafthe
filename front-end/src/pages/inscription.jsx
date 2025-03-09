461051377b86858a9f3d11697b6f66d9731e1271
poupouenpls <aurel.hasteur@gmail.com>
2025-03-09 18:06:39 +0100

    feat: ajout composant inscription

--- front-end/src/pages/inscription.jsx
+++ front-end/src/pages/inscription.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import { XCircleIcon } from '@heroicons/react/20/solid'
 import axios from 'axios'
+import ChampInscription from '../components/ChampInscription'
 
 const Inscription = () => {
     const [utilisateurs_nom, set_utilisateurs_nom] = useState('')
@@ -63,102 +63,19 @@ const Inscription = () => {
             <div className="mt-[42px] sm:mx-auto sm:w-full sm:max-w-[480px]">
                 <div className="bg-white px-[24px] py-[48px] shadow-sm sm:rounded-lg sm:px-[48px]">
                     <form onSubmit={handleSubmit} className="space-y-6">
-
-                        {message && (
-                            <div className="rounded-md bg-red-50 p-4">
-                                <div className="flex">
-                                    <div className="shrink-0">
-                                        <div className="flex">
-                                            <XCircleIcon aria-hidden="true" className="size-5 text-red-400" /> 
-                                            <p className="ml-[10px] text-sm font-medium text-red-800">{message}</p>
-                                        </div>
-                                    </div>
-                                </div>  
-                            </div>
-                        )}
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Nom
-                            </label>
-                            
-                            <div className="mt-[10px]">
-                                <input
-                                    placeholder="Nom"
-                                    type="text"
-                                    value={utilisateurs_nom} 
-                                    onChange={handleChangeNom}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Prénom
-                            </label>
-
-                            <div className="mt-[10px]">                
-                                <input 
-                                    placeholder="Prénom"
-                                    type="text"
-                                    value={utilisateurs_prenom}
-                                    onChange={handleChangePrenom}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>    
-                        </div>
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Adresse Email
-                            </label>
-                            
-                            <div className="mt-[10px]">
-                                <input 
-                                    placeholder="Adresse Email"
-                                    type="email"
-                                    value={utilisateurs_adresse_email}
-                                    onChange={handleChangeAdresseEmail}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Mot de passe
-                            </label>
-                            <div className="mt-[10px]">
-                                <input 
-                                    placeholder="Mot de passe"
-                                    type="password"
-                                    value={utilisateurs_mot_de_passe}
-                                    onChange={handleChangeMotDePasse}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Numéro de téléphone
-                            </label>
-                            <div className="mt-[10px]">
-                                <input
-                                    placeholder="Numéro de téléphone"
-                                    type="text"
-                                    value={utilisateurs_numero_de_telephone}
-                                    onChange={handleChangeNumeroDeTelephone}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>
-                        </div>
+                        <ChampInscription 
+                            utilisateurs_nom={utilisateurs_nom}
+                            utilisateurs_prenom={utilisateurs_prenom}
+                            utilisateurs_adresse_email={utilisateurs_adresse_email}
+                            utilisateurs_mot_de_passe={utilisateurs_mot_de_passe}
+                            utilisateurs_numero_de_telephone={utilisateurs_numero_de_telephone}
+                            handleChangeNom={handleChangeNom}
+                            handleChangePrenom={handleChangePrenom}
+                            handleChangeAdresseEmail={handleChangeAdresseEmail}
+                            handleChangeMotDePasse={handleChangeMotDePasse}
+                            handleChangeNumeroDeTelephone={handleChangeNumeroDeTelephone}
+                            message={message}
+                        />
 
                         <button
                             className="flex w-full justify-center rounded-md bg-emerald-600 px-[16px] py-[12px] font-semibold text-white shadow-xs hover:bg-emerald-500"
