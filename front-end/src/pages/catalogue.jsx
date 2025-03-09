de7bad9aa0820b71d38cc8ec670f316b10957680
poupouenpls <aurel.hasteur@gmail.com>
2025-03-09 17:57:27 +0100

    feat: ajout du composant pour afficher les produits

--- front-end/src/pages/catalogue.jsx
+++ front-end/src/pages/catalogue.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
 import axios from 'axios'
 import Recherche from '../components/recherche'
 import Filtre from '../components/Filtre'
+import Produit from '../components/produit'
 
 const Catalogue = () => {
     const [data, setData] = useState([]) // variable pour stocker et afficher les produits
@@ -58,26 +58,8 @@ const Catalogue = () => {
                     <Filtre handleCategory={handleCategory} activeBouton={activeBouton} />
 
                     <div className="flex-1">
-                        <div className="flex flex-wrap gap-x-6 gap-y-10 w-full">
-                            {filteredProducts.map((produit) => (
-                                <div key={produit.produit_id} className="group w-full sm:w-[calc(50%-12px)] lg:w-[calc(33.333%-16px)]">
-                                <Link to={`/produits/${produit.produit_id}`}>
-                                        <div className="aspect-square w-full overflow-hidden rounded-lg bg-gray-200">
-                                            <img
-                                                src="./image.jpg"
-                                                alt="photo du produit"
-                                                className="h-full w-full object-cover object-center group-hover:opacity-50"
-                                            />
-                                        </div>
-                                        
-                                        <div className="mt-4">
-                                            <h3 className="text-[16px] text-gray-700">{produit.produit_nom}</h3>
-                                            <p className="mt-1 text-lg font-medium text-gray-900">{produit.produit_prix}€</p>
-                                        </div>
-                                    </Link>
-                                </div>
-                            ))}
-                        </div>
+                        {/* On defini le composant produit, on fait passer la fonction filteredProduits dans le props */}
+                        <Produit produits={filteredProducts} />
                     </div>
                 </div>
             </div>
