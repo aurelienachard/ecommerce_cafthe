2dbfd795f2c47c7a6bb790784b3a492d53f86f1a
poupouenpls <aurel.hasteur@gmail.com>
2025-03-09 14:51:37 +0100

    feat: modification du design

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -22,40 +22,36 @@ const Header = () => {
     }
 
     return (
-        <header className="bg-white">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8">
+        <header className="bg-emerald-800">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between gap-x-[24px] p-[24px] lg:px-[32px]">
                 <div className="flex lg:flex-1">
-                    <Link to="/" className="-m-1.5 p-1.5">
-                        <p className="sr-only">Accueil</p>
-                        <img 
-                            src="../logo.png" 
-                            alt="logo de la boutique"
-                            className="h-8 w-auto"
-                        />
+                    <Link to="/">
+                        <p className="text-white font-[inter] font-bold text-[32px]">CAFTHE</p>
                     </Link>
                 </div>
 
-                <div className="hidden lg:flex lg:gap-x-12">
-                    <Link to="/produits" className="text-sm font-semibold leading-6 text-gray-900">
+                <div className="hidden lg:flex lg:gap-x-[48px]">
+                    <Link to="/produits" className="text-base font-semibold text-white font-[inter]">
                         Produits
                     </Link>
-                    <Link to="/panier" className="text-sm font-semibold leading-6 text-gray-900">
+
+                    <Link to="/panier" className="text-base font-semibold text-white font-[inter]">
                         Panier
                     </Link>
                 </div>
 
-                <div className="flex flex-1 items-center justify-end gap-x-6">
+                <div className="flex flex-1 items-center justify-end gap-x-[24px]">
                     {user ? (
                         <>
                             <Link 
                                 to="/profile"
-                                className="hidden lg:block text-sm font-semibold leading-6 text-gray-900">
+                                className="hidden lg:block text-base font-semibold text-white font-[inter]">
                                 Profile
                             </Link>
 
                             <button
                                 onClick={handleDeconnexion}
-                                className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500  focus-visible:outline-offset-2 focus-visible:outline-green-600">
+                                className="rounded-md bg-emerald-600 px-[16px] py-[12px] text-base font-semibold text-white font-[inter] shadow-sm hover:bg-emerald-500 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
                                 Deconnexion
                             </button>
                         </>
@@ -63,13 +59,13 @@ const Header = () => {
                         <>
                             <Link
                                 to="/connexion"
-                                className="hidden lg:block text-sm font-semibold leading-6 text-gray-900">
+                                className="hidden lg:block text-base font-semibold text-white font-[inter]">
                                 Connexion
                             </Link>
 
                             <Link
                                 to="/inscription"
-                                className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500  focus-visible:outline-offset-2 focus-visible:outline-green-600">
+                                className="rounded-md bg-emerald-600 px-[16px] py-[12px] text-base font-semibold text-white font-[inter] shadow-sm hover:bg-emerald-500 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">
                                 Inscription
                             </Link>
                         </>
@@ -81,7 +77,7 @@ const Header = () => {
                     <button
                         type="button"
                         onClick={() => setMobileMenuOpen(true)}
-                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white font-[inter]"
                     >
                         <span className="sr-only">Ouvrir le menu</span>
                         <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
@@ -93,35 +89,27 @@ const Header = () => {
 
             {/* Mobile menu */}
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <div className="fixed inset-0 z-10 bg-black/30" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-emerald-800 px-6 py-6 sm:max-w-sm">
                     <div className="flex items-center gap-x-6">
-                        <Link to="/" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Accueil</span>
-                            <img
-                                src="../logo.png"
-                                alt="logo de la boutique"
-                                className="h-8 w-auto"
-                            />
-                        </Link>
                         {user ? (
                             <button
                                 onClick={handleDeconnexion}
-                                className="ml-auto rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500  focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                                className="ml-auto rounded-md bg-emerald-600 px-[16px] py-[12px] text-base font-semibold text-white font-[inter] shadow-sm hover:bg-emerald-500 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
                             >
                                 Deconnexion
                             </button>
                         ) : (
                             <Link
                                 to="/inscription"
-                                className="ml-auto rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500  focus-visible:outline-offset-2 focus-visible:outline-green-600"
+                                className="ml-auto rounded-md bg-emerald-600 px-[16px] py-[12px] text-base font-semibold text-white font-[inter] shadow-sm hover:bg-emerald-500 focus-visible:outline-offset-2 focus-visible:outline-emerald-600"
                             >
                                 Inscription
                             </Link>
                         )}
                         <button
                             type="button"
-                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                            className="-m-2.5 rounded-md p-2.5 text-white font-[inter] hover:text-emerald-300"
                             onClick={() => setMobileMenuOpen(false)}
                         >
                             <span className="sr-only">Fermer le menu</span>
@@ -130,19 +118,20 @@ const Header = () => {
                             </svg>
                         </button>
                     </div>
+                    
                     <div className="mt-6 flow-root">
-                        <div className="-my-6 divide-y divide-gray-500/10">
+                        <div className="-my-6">
                             <div className="space-y-2 py-6">
                                 <Link
                                     to="/produits"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-[16px] py-[12px] text-base font-semibold text-white font-[inter] hover:bg-emerald-700"
                                     onClick={() => setMobileMenuOpen(false)}
                                 >
                                     Produits
                                 </Link>
                                 <Link
                                     to="/panier"
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                    className="-mx-3 block rounded-lg px-[16px] py-[12px] text-base font-semibold text-white font-[inter] hover:bg-emerald-700"
                                     onClick={() => setMobileMenuOpen(false)}
                                 >
                                     Panier
@@ -150,7 +139,7 @@ const Header = () => {
                                 {user && (
                                     <Link
                                         to="/profile"
-                                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        className="-mx-3 block rounded-lg px-[16px] py-[12px] text-base font-semibold text-white font-[inter] hover:bg-emerald-700"
                                         onClick={() => setMobileMenuOpen(false)}
                                     >
                                         Profile
@@ -161,7 +150,7 @@ const Header = () => {
                                 <div className="py-6">
                                     <Link
                                         to="/connexion"
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                                        className="-mx-3 block rounded-lg px-[16px] py-[12px] text-base font-semibold text-white font-[inter] hover:bg-emerald-700"
                                         onClick={() => setMobileMenuOpen(false)}
                                     >
                                         Connexion
