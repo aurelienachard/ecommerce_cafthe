9bc234ea9cd8b3ea83dede52201740b2e7efcc68
poupouenpls <aurel.hasteur@gmail.com>
2025-03-09 18:04:04 +0100

    feat: ajout composant connexion

--- front-end/src/pages/connexion.jsx
+++ front-end/src/pages/connexion.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import { XCircleIcon } from '@heroicons/react/20/solid'
 import axios from 'axios'
+import ChampConnexion from '../components/champConnexion'
 
 // on utilise setUser en parametre de la fonction pour mettre a jour 
 
@@ -54,53 +54,13 @@ const Connexion = () => {
             <div className="mt-[42px] sm:mx-auto sm:w-full sm:max-w-[480px]">
                 <div className="bg-white px-[24px] py-[48px] shadow-sm sm:rounded-lg sm:px-[48px]">
                     <form onSubmit={handleSubmit} className="space-y-6">
-                        
-                        {message && (
-                            <div className="rounded-md bg-red-50 p-4">
-                                <div className="flex">
-                                    <div className="shrink-0">
-                                        <div className="flex">
-                                            <XCircleIcon aria-hidden="true" className="size-5 text-red-400" /> 
-                                            <p className="ml-[10px] text-sm font-medium text-red-800">{message}</p>
-                                        </div>
-                                    </div>
-                                </div>  
-                            </div>
-                        )}
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Adresse Email
-                            </label>
-
-                            <div className="mt-[10px]">
-                                <input 
-                                    placeholder="Adresse Email" 
-                                    type="mail"
-                                    value={utilisateurs_adresse_email} 
-                                    onChange={handleChangeEmail}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>
-                        </div>
-
-                        <div>
-                            <label className="block text-[16px] font-medium text-gray-900">
-                                Mot de passe
-                            </label>
-
-                            <div className="mt-[10px]">
-                                <input 
-                                    placeholder="Mot de passe" 
-                                    type="password" 
-                                    value={utilisateurs_mot_de_passe} 
-                                    onChange={handleChangePassword}
-                                    className="text-[16px] outline-1 -outline-offset-1 outline-gray-300 block w-full rounded-md px-[12px] py-[16px] bg-white text-base text-gray-900 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-emerald-600 sm:text-[14px]"
-                                    required
-                                />
-                            </div>
-                        </div>
+                        <ChampConnexion 
+                            utilisateurs_adresse_email={utilisateurs_adresse_email}
+                            utilisateurs_mot_de_passe={utilisateurs_mot_de_passe}
+                            handleChangeEmail={handleChangeEmail}
+                            handleChangePassword={handleChangePassword}
+                            message={message}
+                        />
 
                         <div className="text-[16px]">
                             <Link 
