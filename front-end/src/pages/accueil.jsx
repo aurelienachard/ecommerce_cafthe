0275c46d3940e15bc803c50c3e92e4f51ebb8f9e
poupouenpls <aurel.hasteur@gmail.com>
2025-03-11 15:31:53 +0100

    feat: configuration de l'environnement complet pour les tests unitaires

--- front-end/src/pages/accueil.jsx
+++
@@ -1,9 +0,0 @@
-const Accueil = () => {
-    return (
-        <div className="p-[24px] min-h-screen">
-            <h1 className="text-[32px] font-bold">Bievenue sur Cafthe.</h1>
-        </div>
-    )
-}
-
-export default Accueil
--- front-end/testSetup.js
+++ front-end/testSetup.js
@@ -2,6 +2,8 @@ import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
+// fonction qui permet de réinitialiser jsdom, qui simule le navigateur
+
 afterEach(() => {
   cleanup()
 })
