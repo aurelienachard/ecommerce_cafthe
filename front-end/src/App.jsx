b1ba27fd80f166992c1e9c3be569877f062f949b
aurelienachard <aurelien.achard@proton.me>
2025-04-06 20:44:19 +0200

    fix: modification du browser router a cause d'un probleme d'acces sur le deploiement

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
 import Catalogue from "./pages/catalogue"
 import Produit from "./pages/produit"
